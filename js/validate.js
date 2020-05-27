98069eecb6cc8e6661a4040e41075c53fc04f1c7
martinvveselinov <49569108+martinvveselinov@users.noreply.github.com>
2020-05-27 19:11:27 +0300

    admin panel added

--- js/validate.js
+++ js/validate.js
@@ -1,16 +1,11 @@
 function validate(){
 	var email = document.forms["login-form"]["email"].value;
 	var pass = document.forms["login-form"]["pass"].value;
-	alert(email);
-	alert(pass);
 	function clear(str) {
 		document.getElementById(str).value = "";
 		document.getElementById(str).style.color = currColor;
 	};
-	if(email === "admin@admin.bg" && pass === "admin"){
-		location.href='./infopage.html';
-	}	
-	else if(email !== "user@user.bg" && email !== "admin@admin.bg"){
+	if(email !== "user@user.bg"){
 		var currColor = document.getElementById('email').style.color;
 		document.getElementById('email').style.color = "red";
 		document.getElementById('email').value = "That email address is not registered!";
@@ -24,6 +19,34 @@ function validate(){
 		return false;
 	}
 	else if(email === "user@user.bg" && pass === "pass"){
-		location.href='./home.html';
+		alert("home");
+		location.href='./html/home.html';
+		return false;
+	}
+}
+function validateAdmin(){
+	var email = document.forms["admin-login-form"]["email"].value;
+	var pass = document.forms["admin-login-form"]["pass"].value;
+	function clear(str) {
+		document.getElementById(str).value = "";
+		document.getElementById(str).style.color = currColor;
+	};
+	if(email !== "admin@admin.bg"){
+		var currColor = document.getElementById('email').style.color;
+		document.getElementById('email').style.color = "red";
+		document.getElementById('email').value = "That email address is not a valid administrator's email!";
+		document.getElementById('email').onclick=function() {clear('email')};
+		return false;		
+	}
+	else if(pass !== "admin"){
+		var currColor = document.getElementById('pass').style.color;
+		document.getElementById('pass').style.color = "red";
+		document.getElementById('pass').onclick=function() {clear('pass')};
+		return false;
+	}
+	else if(email === "admin@admin.bg" && pass === "admin"){
+		alert("admin");
+		location.href='./html/admin-panel.html';
+		return false;
 	}
 }
